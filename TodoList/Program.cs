1b4705e905278c6b99a7705f0fd6eacdfce69d89
Gustav Polhem <gustavpolhem@gmail.com>
2024-08-20 22:37:09 +0200

    added typedResults in program.cs

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,6 +1,7 @@
 using TodoList.Data;
 using TodoList.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,14 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext") ??
 
 
 var app = builder.Build();
+
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", GetAllTodoItems);
+todoItems.MapGet("/{id}", GetTodoItem);
+todoItems.MapPost("/", CreateTodoItem);
+todoItems.MapPut("/{id}", UpdateTodoItem);
+todoItems.MapDelete("/{id}", DeleteTodoItem);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -32,17 +41,52 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
-app.MapGet("/todoitems", async (TodoContext db) =>
-    await db.TodoItems.ToListAsync());
-
-
-app.MapPost("/todoitems", async (TodoContext db, TodoItem todo) =>
-    {
-        db.TodoItems.Add(todo);
-        await db.SaveChangesAsync();
-        return Results.Created($"/todoitems/{todo.Id}", todo);
-    });
-
-
-
 app.Run();
+
+
+static async Task<IResult> GetAllTodoItems(TodoContext db)
+{
+    return TypedResults.Ok(await db.TodoItems.ToArrayAsync());
+}
+
+static async Task<IResult> GetTodoItem(TodoContext db, int id)
+{
+    return await db.TodoItems.FindAsync(id)
+        is TodoItem todo
+            ? TypedResults.Ok(todo)
+            : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodoItem(TodoContext db, TodoItem todo)
+{
+    db.TodoItems.Add(todo);
+    await db.SaveChangesAsync();
+    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+}
+
+static async Task<IResult> UpdateTodoItem(TodoContext db, TodoItem inputTodo, int id)
+{
+    var todo = await db.TodoItems.FindAsync(id);
+
+    if (todo is null) return TypedResults.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> DeleteTodoItem(TodoContext db, int id)
+{
+    if (await db.TodoItems.FindAsync(id) is TodoItem todo)
+    {
+        db.TodoItems.Remove(todo);
+        db.SaveChanges();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
+
+
+}
